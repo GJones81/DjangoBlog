c5e6ed4e9731f1afb4a057260ce623cf811f7137
gjones81 <jones.guy81@gmail.com>
2020-10-20 17:23:43 -0700

    wrote more of the blog views/urls

--- BlogCloneProject/blog/views.py
+++ BlogCloneProject/blog/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
-from blog.models import Post, Comment
-from blog.forms import PostForm, CommentForm
-from django.urls import reverse_lazy
-from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 # Mixins are classes that add functionality like decorators
 from django.contrib.auth.mixins import LoginRequiredMixin
+from blog.forms import PostForm, CommentForm
+from blog.models import Post, Comment
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy
+from django.utils import timezone
 from django.views.generic import (TemplateView, ListView,
                                   DetailView, CreateView,
                                   UpdateView, DeleteView,)
@@ -54,3 +55,39 @@ class DraftListView(LoginRequiredMixin ,ListView):
 
     def get_queryset(self):
         return Post.objects.filter(published_date__isnull=True).order_by('created_date')
+
+@login_required
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            # comment.post is set equal to the post object from models.py
+            comment.post = post
+            comment.save()
+            # pk refers to Primary Key
+            return redirect('post_detail', pk=post.pk)
+    else:
+        # the CommentForm object from forms.py
+        form = CommentForm()
+    return render(request, 'blog/comment_form.html', {'form': form})
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail',pk=post_pk)
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish
+    return redirect('post_detail', pk=pk)
